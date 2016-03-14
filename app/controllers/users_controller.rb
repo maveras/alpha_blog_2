2f8046759ce9793d4c31ef766b8e496382ece8f5
Marcelo Vera <marcelo.verasalvatierra@gmail.com>
2016-03-14 13:34:24 +0000

    Update curren_user to be article creator

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
     
     def require_same_user
         if current_user != @user
-            flash[:danger] = "You can only edit your account"
+            flash[:danger] = "You can only edit your own account"
             redirect_to root_path
         end
     end
