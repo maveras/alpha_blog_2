0b10138f9e4a22c4db2463ae19eb0a20e4d1a96c
Marcelo Vera <marcelo.verasalvatierra@gmail.com>
2016-04-07 19:52:55 +0000

    Update all views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -61,7 +61,7 @@ class ArticlesController < ApplicationController
     
     #creo un metodo que lo paso al momento de crear el articulo (para hacerla corta)
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description, category_ids: [])
     end
     
     def require_same_user
